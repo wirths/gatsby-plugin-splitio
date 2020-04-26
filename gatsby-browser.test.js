d4d600fddbbd8415900f64dedf6d8acad994d0a1
Harald T. Wirths <harald.wirths@4fb.de>
2020-04-26 15:28:20 +0200

    feat: add an initial test

---
+++ gatsby-browser.test.js
@@ -0,0 +1,18 @@
+//import React from 'react'
+import renderer from "react-test-renderer";
+
+import { wrapRootElement } from "./gatsby-browser";
+
+describe("SplitFactory wraps Gatsby root element", () => {
+	it("contains the wrapped state", () => {
+		const configData = {
+			authorizationKey: "heiner",
+			key: "hansen",
+			plugins: [],
+		};
+		const wrappedElement = wrapRootElement(`<></>`, configData);
+
+		const tree = renderer.create(wrappedElement).toTree();
+		expect(tree.instance.state).toHaveProperty("isReady");
+	});
+});
